2fb20d4cbd7e541e6c27792be6a985be241ab2d6
Mark Gaither <mark@arowabove.com>
2018-06-29 13:35:56 -0700

    updated across the board

--- worksheets/raft_dp_campaigns.sql
+++ worksheets/raft_dp_campaigns.sql
@@ -1,19 +1,29 @@
 -- dp campaigns
 use dp;
 
+-- create a new local db table to used later
+if (object_id('dbo.DPCAMPAIGNS') is not null) begin drop table dbo.DPCAMPAIGNS end;
+
 if (object_id('tempdb..#campaigns') is not null) begin drop table #campaigns end;
-select distinct a.campaign into #campaigns
+select distinct a.campaign as id into #campaigns
 from dpgift a 
 where a.campaign != ''
 order by a.campaign;
 
--- select * from #campaigns order by campaign;
+--select * from #campaigns order by id;
 
 -- dump campaigns
-select '012f4000000iS0Z' as RecordTypeId
-	, isnull(b.campaign,'') as Name
-from dpgift a
-left join #campaigns b on a.campaign = b.campaign
-where a.record_type in ('P','G')
-	and a.campaign != ''
-order by b.campaign;
+-- Donor_Perfect_Id__c,Name,Status,Type,IsActive,RecordTypeId
+select a.id as Donor_Perfect_Id__c
+	, isnull(b.description,a.id) as Name
+	, 'Completed' as Status
+	, 'Event' as Type
+	, '1' as IsActive
+	, '012f4000000iS0Z' as RecordTypeId
+into DPCAMPAIGNS
+from #campaigns a
+inner join dpcodes b on a.id = b.code
+where b.field_name = 'CAMPAIGN'
+order by a.id;
+
+
--- worksheets/raft_dp_gifts.sql
+++ worksheets/raft_dp_gifts.sql
@@ -1,19 +1,92 @@
 -- dp gift to opportunity
 use dp;
 
--- dump gifts
--- Donor_Perfect_Id__c,Donor_Perfect_Contact_Id__c,Donor_Perfect_Account_Id__c,StageName,CloseDate,Amount,Donor_Perfect_Campaign_Id__c,Description
-select top 5 a.gift_id as Donor_Perfect_Id__c
+-- update dpgift table
+alter table dpgift alter column gift_date datetime2 not null;
+alter table dpgift alter column gift_id int not null;
+alter table dpgift alter column donor_id int not null;
+select * from dpgift order by gift_id desc;
+
+-- test queries
+select top 100 a.gift_id as Donor_Perfect_Id__c
 	, a.donor_id as Donor_Perfect_Contact_Id__c
 	, a.donor_id as Donor_Perfect_Account_Id__c
 	, case
 		when a.record_type = 'G' then 'Posted'
 		when a.record_type = 'P' then 'Pledged'
 	  end as StageName
-	, isnull(convert(nvarchar,a.gift_date,126),'') as CloseDate
+	, isnull(convert(nvarchar,a.gift_date,110),'') as CloseDate
 	, a.amount as Amount
-	, a.campaign as Donor_Perfect_Campaign_Id__c
+	, isnull(b.donor_perfect_id__c,'') as Donor_Perfect_Campaign_Id__c
 	, isnull(a.gift_narrative,'') as Description
+	, case
+		when c.donor_type in ('IN','INDIV3') or c.donor_type = '' then concat(c.first_name,' ',c.last_name,' Purchase ',isnull(convert(nvarchar,a.gift_date,110),''))
+		when c.donor_type in ('BIZ','BUSIN1','CO','FN','FOUND2','JO','OR','ORGAN4') then concat(c.last_name,' Purchase ',isnull(convert(nvarchar,a.gift_date,110),''))
+	  end as Name
 from dpgift a
+left join dpcampaigns b on a.campaign = b.donor_perfect_id__c
+left join dp c on a.donor_id = c.donor_id
 where a.record_type in ('P','G')
-order by a.gift_id;
+	and a.campaign = 'UPCYC17'
+	and a.donor_id = 9
+order by a.record_type;
+
+select campaign,record_type,donor_id, count(*) as count
+from dpgift
+group by campaign,record_type,donor_id
+order by campaign;
+
+
+select * from dpgift where donor_id = 603;
+
+
+-- dump gifts that are posted
+-- Donor_Perfect_Id__c,Donor_Perfect_Contact_Id__c,Donor_Perfect_Account_Id__c,StageName,CloseDate,Amount,Donor_Perfect_Campaign_Id__c,Description,Name
+select a.gift_id as Donor_Perfect_Id__c
+	, a.donor_id as Donor_Perfect_Contact_Id__c
+	, a.donor_id as Donor_Perfect_Account_Id__c
+	, case
+		when a.record_type = 'G' then 'Posted'
+		when a.record_type = 'P' then 'Pledged'
+	  end as StageName
+	, isnull(convert(nvarchar,a.gift_date,110),'') as CloseDate
+	, a.amount as Amount
+	, isnull(b.donor_perfect_id__c,'') as Donor_Perfect_Campaign_Id__c
+	, isnull(a.gift_narrative,'') as Description
+	, case
+		when c.donor_type in ('IN','INDIV3') or c.donor_type = '' then concat(c.first_name,' ',c.last_name,' Purchase ',isnull(convert(nvarchar,a.gift_date,110),''))
+		when c.donor_type in ('BIZ','BUSIN1','CO','FN','FOUND2','JO','OR','ORGAN4') then concat(c.last_name,' Purchase ',isnull(convert(nvarchar,a.gift_date,110),''))
+	  end as Name
+from dpgift a
+left join dpcampaigns b on a.campaign = b.donor_perfect_id__c
+left join dp c on a.donor_id = c.donor_id
+--where a.record_type = 'G'
+where a.record_type = 'G'
+	and a.campaign != ''
+order by a.donor_id;
+
+-- dump gifts that are pledged
+-- Donor_Perfect_Id__c,Donor_Perfect_Contact_Id__c,Donor_Perfect_Account_Id__c,StageName,CloseDate,Amount,Donor_Perfect_Campaign_Id__c,Description,Name
+select a.gift_id as Donor_Perfect_Id__c
+	, a.donor_id as Donor_Perfect_Contact_Id__c
+	, a.donor_id as Donor_Perfect_Account_Id__c
+	, case
+		when a.record_type = 'G' then 'Posted'
+		when a.record_type = 'P' then 'Pledged'
+	  end as StageName
+	, isnull(convert(nvarchar,a.gift_date,110),'') as CloseDate
+	, a.amount as Amount
+	, isnull(b.donor_perfect_id__c,'') as Donor_Perfect_Campaign_Id__c
+	, isnull(a.gift_narrative,'') as Description
+	, case
+		when c.donor_type in ('IN','INDIV3') or c.donor_type = '' then concat(c.first_name,' ',c.last_name,' Pledge ',isnull(convert(nvarchar,a.gift_date,110),''))
+		when c.donor_type in ('BIZ','BUSIN1','CO','FN','FOUND2','JO','OR','ORGAN4') then concat(c.last_name,' Pledge ',isnull(convert(nvarchar,a.gift_date,110),''))
+	  end as Name
+from dpgift a
+left join dpcampaigns b on a.campaign = b.donor_perfect_id__c
+left join dp c on a.donor_id = c.donor_id
+where a.record_type = 'P'
+	and a.campaign != ''
+order by a.donor_id;
+
+
