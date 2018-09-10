0184e393d8768c8b2dc6eaf0514c775e2c167910
Mark Gaither <mark@arowabove.com>
2018-09-09 21:32:42 -0700

    added donations.sql; updated roi.sql

--- analytics/roi.sql
+++ analytics/roi.sql
@@ -1,15 +1,65 @@
 use fww;
+set nocount on
+
+-- count variables
+declare @segmentcountmax int
+declare @segmentcount int
+declare @segmentdaycount int
 
 
 
--- 686,638
-select count(a.id)
-from contacts a
-inner join opportunities b on a.id = b.npsp__Primary_Contact__c;
+/*********** DONATION SEGMENTS ***********/
+set @segmentdaycount = 90;
+-- segments
+-- 2017-09-14, 2017-12-13, 2018-03-13, 2018-06-11, 2018-09-09
+--select dateadd(day,-@segmentdaycount*4,getdate()), dateadd(day,-@segmentdaycount*3,getdate()), dateadd(day,-@segmentdaycount*2,getdate()), dateadd(day,-@segmentdaycount,getdate()), getdate();
 
--- 686,638
-select a.npsp__Primary_Contact__c, b.id
-from opportunities a
-left join contacts b on a.npsp__Primary_Contact__c = b.id
-where a.npsp__Primary_Contact__c != ''
-order by a.npsp__Primary_Contact__c;
+if (object_id('tempdb..#segment1') is not null) begin drop table #segment1 end;
+select id
+	, channel
+	, type
+	, joineddate
+	, amount
+into #segment1 
+from source_donations
+where joineddate >= dateadd(day,-@segmentdaycount,getdate());
+-- select * from #segment1;
+
+if (object_id('tempdb..#segment2') is not null) begin drop table #segment2 end;
+select id
+	, channel
+	, type
+	, joineddate
+	, amount
+into #segment2 
+from source_donations
+where joineddate >= dateadd(day,-@segmentdaycount*2,getdate())
+	and joineddate < dateadd(day,-@segmentdaycount,getdate());
+-- select * from #segment2;
+
+if (object_id('tempdb..#segment3') is not null) begin drop table #segment3 end;
+select id
+	, channel
+	, type
+	, joineddate
+	, amount
+into #segment3 
+from source_donations
+where joineddate >= dateadd(day,-@segmentdaycount*3,getdate())
+	and joineddate < dateadd(day,-@segmentdaycount*2,getdate());
+-- select * from #segment3;
+
+if (object_id('tempdb..#segment4') is not null) begin drop table #segment4 end;
+select id
+	, channel
+	, type
+	, joineddate
+	, amount
+into #segment4 
+from source_donations
+where joineddate >= dateadd(day,-@segmentdaycount*4,getdate())
+	and joineddate < dateadd(day,-@segmentdaycount*3,getdate());
+-- select * from #segment4 order by joineddate;
+
+
+set nocount off
--- analytics/sources.sql
+++ analytics/sources.sql
@@ -3,14 +3,87 @@ use fww;
 -- alter table queries
 alter table sources alter column C4_Date_Joined__c date;
 alter table sources alter column C3_Date_Joined__c date;
+alter table sources alter column CreatedDate date;
+alter table sources add JoinedDate date;
 alter table sources alter column total_combined_credits_lifetime__c float not null;
 alter table sources alter column total_hard_credits_lifetime__c float not null;
 alter table sources alter column total_soft_credits_lifetime__c float not null;
+--================== UPDATE QUERIES ===============
 update sources set C4_Date_Joined__c = null where C4_Date_Joined__c = '1900-01-01';
 update sources set C3_Date_Joined__c = null where C3_Date_Joined__c = '1900-01-01';
+-- populate the JoinedDate column
+--select count(*) from sources where CreatedDate is null; -- 0
+--select count(*) from sources where C3_Date_Joined__c is null and C4_Date_Joined__c is not null; -- 42,689
+--select count(*) from sources where C3_Date_Joined__c is not null and C4_Date_Joined__c is null; -- 1,706,450
+--select count(*) from sources where C3_Date_Joined__c is null and C4_Date_Joined__c is null; -- 252.748
+-- find earliest date between these three dates: CreatedDate, C3_Date_Joined__c, C4_Date_Joined__c
+update sources
+set 
+JoinedDate =
+		case
+			when CreatedDate < C3_Date_Joined__c and CreatedDate < C4_Date_Joined__c and C3_Date_Joined__c < C4_Date_Joined__c then CreatedDate
+			when CreatedDate < C4_Date_Joined__c and CreatedDate < C3_Date_Joined__c and C4_Date_Joined__c < C3_Date_Joined__c then CreatedDate
+			when C3_Date_Joined__c < CreatedDate and C3_Date_Joined__c < C4_Date_Joined__c and CreatedDate < C4_Date_Joined__c then C3_Date_Joined__c
+			when C3_Date_Joined__c < C4_Date_Joined__c and C3_Date_Joined__c < CreatedDate and C4_Date_Joined__c < CreatedDate then C3_Date_Joined__c
+			when C4_Date_Joined__c < CreatedDate and C4_Date_Joined__c < C3_Date_Joined__c and CreatedDate < C3_Date_Joined__c then C4_Date_Joined__c
+			when C4_Date_Joined__c < C3_Date_Joined__c and C4_Date_Joined__c < CreatedDate and C3_Date_Joined__c < CreatedDate then C4_Date_Joined__c
+			else CreatedDate
+		end
+where 1 = 1
+	and CreatedDate is not null
+	and C3_Date_Joined__c is not null
+	and C4_Date_Joined__c is not null;
+
+update sources set 
+--select CreatedDate, C4_Date_Joined__c,
+JoinedDate =
+		case
+			when CreatedDate < C4_Date_Joined__c then CreatedDate
+			when C4_Date_Joined__c < CreatedDate then C4_Date_Joined__c
+			else CreatedDate
+		end
+--from sources
+where 1 = 1
+	and CreatedDate is not null
+	and C3_Date_Joined__c is null
+	and C4_Date_Joined__c is not null;
+
+update sources set 
+--select CreatedDate, C4_Date_Joined__c,
+JoinedDate =
+		case
+			when CreatedDate < C3_Date_Joined__c then CreatedDate
+			when C3_Date_Joined__c < CreatedDate then C3_Date_Joined__c
+			else CreatedDate
+		end
+--from sources
+where 1 = 1
+	and CreatedDate is not null
+	and C3_Date_Joined__c is not null
+	and C4_Date_Joined__c is null;
+
+update sources set 
+--select CreatedDate, C4_Date_Joined__c,
+JoinedDate = CreatedDate
+--from sources
+where 1 = 1
+	and CreatedDate is not null
+	and C3_Date_Joined__c is null
+	and C4_Date_Joined__c is null;
+--================== UPDATE QUERIES ===============
+
+-- this should be zero
+select count(*) from sources where JoinedDate is null;
+-- make sure JoinedDate is the least recent date
+select JoinedDate, CreatedDate, C3_Date_Joined__c, C4_Date_Joined__c 
+from sources
+where 1 = 1
+	and (JoinedDate <= CreatedDate 
+	or JoinedDate <= C3_Date_Joined__c 
+	or JoinedDate <= C4_Date_Joined__c);
 
 -- sources
-select count(*) from sources;
+select count(*) from sources where JoinedDate is not null;
 
 -- dupe check
 select id, count(*) as dupes
@@ -74,5 +147,8 @@ order by Origin_Source_Code_Channel__c;
 select C3_Date_Joined__c
 	, C4_Date_Joined__c 
 from sources
+where 1 = 1 
+	and C3_Date_Joined__c is not null
+	and C4_Date_Joined__c is not null
 order by C3_Date_Joined__c;
 
